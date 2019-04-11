362df475099ecd1367737f7158f40f7c1a881430
Ricardo Burgos <Ricardo Burgos@DESKTOP-65OB9H8>
2019-04-11 14:34:28 -0600

    Se agrego un archivo index.html(need 2 die fast)

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo 'Hola mundo';
+/*Este es mi primer hola mundo en github, tbh is not but the teacher wants me to say so so yeah*/
